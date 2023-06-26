551fcdbbf39b79d20025f00a08971b22538da930
PC <heidysamanthabc@ufps.edu.co>
2023-06-26 13:13:58 -0500

    corrigiendo errores

--- src/main/java/link/softbond/controller/ExamenController.java
+++ src/main/java/link/softbond/controller/ExamenController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.app.web.entities.Bill;
-
 import link.softbond.entities.Examen;
 import link.softbond.entities.Opcion;
 import link.softbond.entities.Usuario;
