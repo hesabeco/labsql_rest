beab8d341257c0325c072da440f5af16de9d809e
PC <heidysamanthabc@ufps.edu.co>
2023-06-26 12:19:12 -0500

    H16- Se crea en problema controller agregar problema

--- src/main/java/link/softbond/controller/ProblemaController.java
+++ src/main/java/link/softbond/controller/ProblemaController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.web.entities.Bill;
 
 import link.softbond.entities.Problema;
 import link.softbond.repositorios.ProblemaRepository;
@@ -33,4 +37,9 @@ public class ProblemaController {
 		    }
 		    return problemaEstado;
 		}
+	@PostMapping
+	public Problema agregarProblema(@RequestBody Problema problema) {
+		problemaRepository.save(problema);
+		return problema;
+	}
 }
