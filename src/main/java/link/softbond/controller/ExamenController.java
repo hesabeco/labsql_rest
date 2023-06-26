ee3bc77e35eb50fed86226c7cfd14096528affe9
PC <heidysamanthabc@ufps.edu.co>
2023-06-26 12:53:02 -0500

    H18-Se crea en examen controller crear examen

--- src/main/java/link/softbond/controller/ExamenController.java
+++ src/main/java/link/softbond/controller/ExamenController.java
@@ -3,6 +3,8 @@ package link.softbond.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import link.softbond.entities.Examen;
@@ -15,5 +17,11 @@ public class ExamenController {
 public List<Examen> getExamenes() {
 	List<Examen> examenes = examenRepository.findAll();
 	return examenes;
+}
+
+@PostMapping
+public Examen agregarExamen (@RequestBody Examen examen) {
+	examenRepository.save(examen);
+	return examen;
 }
 }
